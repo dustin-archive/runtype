5139f3500b9edf7d219cb8f2fb998b8b49a6e414
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2019-07-31 02:21:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,82 @@
 
-const parse = input => {
-  const start = input.indexOf('(')
-
-  if (start === -1) {
-    let position = input.indexOf('|')
-
-    if (position === -1) {
-      return { type: input } // this is done
+function endsWithBrackets (type) {
+  if (type.endsWith('[]')) {
+    return {
+      type: 'array',
+      arrayTypes: [
+        {
+          type: type.slice(0, -2)
+        }
+      ]
     }
-
-    // temporary solution
-    const target = []
-    const x = input.split('|')
-    for (let i = 0; i < x.length; i++) {
-      target.push({ type: x[i] })
-    }
-    return target
-
-    // let pos = 0
-    //
-    // const target = []
-    // let remaining = input
-    //
-    // while (position !== -1) {
-    //   position = remaining.indexOf('|')
-    //
-    //   target.push({
-    //     type: remaining.slice(0, position)
-    //   })
-    //
-    //   remaining = remaining.slice(position + 1)
-    // }
-    //
-    // return target
   }
 
-  const end = input.lastIndexOf(')[]')
+  return {
+    type: type
+  }
+}
+
+function parse (input) {
+  var start = input.indexOf('(')
+
+  if (start === -1) {
+    var next = input.indexOf('|')
+
+    if (next === -1) {
+      return endsWithBrackets(input)
+    }
+
+    var target = []
+    var prev = 0
+
+    for (var i = 0; next !== -1; i++) {
+      target[i] = endsWithBrackets(input.slice(prev, next))
+      prev = next + 1
+      next = input.indexOf('|', prev)
+    }
+
+    target[i] = endsWithBrackets(input.slice(prev))
+
+    return target
+  }
+
+  var end = input.lastIndexOf(')[]')
 
   if (end === -1) {
     return 'ERROR'
   }
+
+  return {
+    type: 'array',
+    arrayTypes: parse(input.slice(start + 1, end))
+  }
 }
 
-console.log(parse('string'))
-console.log(parse('aa|bbcc|ddeeff'))
-// console.log(parse('string[]'))
-// console.log(parse('string|number'))
-// console.log(parse('(string|number)[]'))
-// console.log(parse('string[]|number[]'))
+function runtype (data, interface) {
+  for (var key in interface) {
+    const list = parse(interface[key])
 
-// const middle = input.slice(start, end)
-//
-// console.log(middle)
+    for (var i = 0; i < list.length; i++) {
+      const item = list[i]
+      const value = data[key]
 
-// const start = indexOf('|')
-//
-// if (start === -1) {
-//   return
-// }
-//
-// const
+      if (typeof value === item.type) {
+        return 'pass'
+      }
+    }
+  }
 
-// const target = {}
-// for (let i = 0; i < input.length; i++) {
-//
-// }
-// return target
+  return 'fail'
+}
+
+var data = {
+  hello: 123
+}
+
+var interface = {
+  hello: 'string|number'
+}
+
+console.log(
+  runtype(data, interface)
+)
